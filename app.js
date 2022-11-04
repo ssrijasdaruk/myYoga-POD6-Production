93ac008dcad0a1cc021b797dac49b8252a55adfa
Lian Ho Yeung <iamhabe@connect.hku.hk>
2022-11-04 17:24:44 +0800

    Installed MSSQL

--- app.js
+++ app.js
@@ -23,12 +23,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -36,6 +36,66 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+var sql = require("mssql");
+
+var config =
+{
+  server: 'myyoga-pod6-server.database.windows.net',
+  user: 'myyoga-pod6-server-admin',
+  password: "!Abcd1234",
+  database: 'myyogapod6-database',
+  ssl: true,
+  port: 1433,
+  options: {
+    encrypt: true,
+    enableArithAbort: true,
+    trustedConnection: true,
+  }
+};
+
+app.post('/signin', (req, res) => {
+  var username = req.body.username;
+  var password = req.body.password;
+  var query = "SELECT UserID, LoginName, IsAdmin FROM [dbo].[User] WHERE LoginName = '" + username + "' AND PasswordHash ='" + password + "';";
+  sql.connect(config, function (err) {
+    var request = new sql.Request();
+    request.query(query, function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(data);
+      if (data.recordset.length > 0) {
+        console.log("Login Success")
+        res.cookie("userID", data.recordset[0].UserID);
+        res.json({ data: data.recordset });
+      }
+      else {
+        res.send(400);
+      }
+
+    });
+  });
+});
+
+app.get("/retrieve", (req, res) => {
+  sql.connect(config, function (err) {
+
+    if (err) {
+      console.log(err);
+    }
+
+    var request = new sql.Request();
+    request.query('SELECT TOP (1000) [ID] ,[Message] FROM [dbo].[Testing_Table]', function (err, data) {
+
+      if (err) {
+        console.log(err)
+      }
+      res.send(200, data.recordset.length);
+      sql.close();
+    });
+  });
 });
 
 module.exports = app;
