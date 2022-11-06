bb9b9438ae5e794a41e450d249c45642961f3dfe
kenneth123321 <kamwing1999@hotmail.com>
2022-11-06 19:36:59 +0800

    redo class + update vacan&voc after book

--- app.js
+++ app.js
@@ -89,6 +89,65 @@ app.get("/retrieve_class", (req, res) => {
   });
 });
 
+
+app.get("/update_vacancies", (req, res) => {
+  var classid = req.query.classid;
+  var vacancies = req.query.vacancies;
+  query = "UPDATE [dbo].[ClassTable] SET CurrVacancies = " + vacancies + " WHERE ClassID = " + classid;
+
+  sql.connect(config, function (err) {
+    console.log("Updating class vacancies");
+
+    if (err) {
+      console.log(err);
+    }
+    else {
+      var request = new sql.Request();
+      request.query(query, function (err, data) {
+
+        if (err) {
+          console.log(err)
+          res.send(500);
+          sql.close();
+        }
+        else {
+          res.send(200);
+          sql.close();
+        }
+      });
+    }
+  });
+});
+
+app.get("/update_vouchers", (req, res) => {
+    var userid = req.query.userid;
+    var vouchers = req.query.vouchers;
+    query = " UPDATE [dbo].[User] SET vouchers = " + vouchers + " WHERE UserID = " + userid;
+
+    sql.connect(config, function (err) {
+      console.log("Updating user vouchers");
+
+      if (err) {
+        console.log(err);
+      }
+      else {
+        var request = new sql.Request();
+        request.query(query, function (err, data) {
+  
+          if (err) {
+            console.log(err)
+            res.send(500);
+            sql.close();
+          }
+          else {
+            res.send(200);
+            sql.close();
+          }
+        });
+      }
+    });
+});
+
 app.get("/insert_class", (req, res) => {
   var table = "[dbo].[ClassTable]";
   var ClassName = req.query.ClassName
@@ -120,6 +179,33 @@ app.get("/insert_class", (req, res) => {
         }
       });
     }
+  });
+});
+
+app.get('/reset_cookies', (req, res) => {
+  var userid = req.query.userid;
+  var query = "SELECT UserID, LoginName, IsAdmin, Vouchers FROM [dbo].[User] WHERE UserID = " + userid ;
+  console.log(query);
+  sql.connect(config, function (err) {
+    var request = new sql.Request();
+    request.query(query, function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(data);
+      if (data.recordset.length > 0) {
+        console.log("Reseting cookies Success")
+        res.cookie("user_id", data.recordset[0].UserID, { maxAge: 600000 });
+        res.cookie("username", data.recordset[0].LoginName, { maxAge: 600000 });
+        res.cookie("isAdmin", data.recordset[0].IsAdmin, { maxAge: 600000 });
+        res.cookie("vouchers", data.recordset[0].Vouchers, { maxAge: 600000 });
+        res.json({ data: data.recordset });
+      }
+      else {
+        res.send(400);
+      }
+
+    });
   });
 });
 // catch 404 and forward to error handler
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -35,24 +35,178 @@ function deleteAllCookies() {
     alert("You are logout");
 }
 
-function setupElement() {
+async function setupElement() {
     let cookie_list = getCookieList();
     var login = document.getElementById("login").classList;
     var logout = document.getElementById("logout").classList;
-    var classtable = document.getElementById("classtable").classList;
     var admin = document.getElementById("admin").classList;
     console.log(cookie_list);
     if(isCookieEmpty()){
         logout.add("hidden");
-        classtable.add("hidden");
+        renderNormalTable();
     }else{
         login.add("hidden");
-
+        await reset_cookies(cookie_list.user_id);
+        await renderMmbTable();
     }
-
 
     if( isCookieEmpty() || cookie_list.isAdmin == 'false'){
         admin.add("hidden");
     }
 }
 
+function renderTable(){
+    if(isCookieEmpty()){
+        renderNormalTable();
+    }else{
+        renderMmbTable();
+    }
+}
+
+async function renderNormalTable(){
+    const res = await retriev_class();
+    
+    if(res != "Error"){
+        var data = JSON.parse(res);
+        var table = document.getElementById('table');
+        var html_str = '<thead><tr><th>Class</th><th>Vacancies</th><th>Time</th><th>Coach</th></tr></thead><tbody>';
+        for( let i = 0; i < data.length ; i++){
+            html_str = html_str + '<tr><td>'
+            + data[i].ClassID + '</td><td>'
+            + data[i].CurrVacancies + '/' + data[i].MaxVacancies + '</td><td>'
+            + data[i].EndDate + '</td><td>'
+            + data[i].Coach + '</td><tr>';
+        }
+        html_str = html_str + '</tbody>';
+        table.innerHTML = html_str;
+    }
+}
+
+async function renderMmbTable(){
+    const cookie_list = getCookieList();
+    var vouchers = document.getElementById('vouchers');
+    vouchers.innerHTML = '<h2 style="text-align: center;"> Voucher(s): ' + cookie_list.vouchers +  '</h2>';
+    const res = await retriev_class();
+    
+    if(res != "Error"){
+        var data = JSON.parse(res);
+        var table = document.getElementById('table');
+        var html_str = '<thead><tr><th>Class</th><th>Vacancies</th><th>Time</th><th>Coach</th><th>Action</th></tr></thead><tbody>';
+        for( let i = 0; i < data.length ; i++){
+            html_str = html_str + '<tr><td>'
+                        + data[i].ClassID + '</td><td>'
+                        + data[i].CurrVacancies + '/' + data[i].MaxVacancies + '</td><td>'
+                        + data[i].EndDate + '</td><td>'
+                        + data[i].Coach + '</td><td>'
+                        + '<a href="javascript:;" onclick="applyCourse(' + 
+                        data[i].ClassID + ',' + cookie_list.user_id + ',' + data[i].CurrVacancies + ',' + data[i].MaxVacancies + ',' + cookie_list.vouchers +
+                        ')">Book</a>';
+
+            if(cookie_list.isAdmin == 'true'){
+                html_str = html_str + '&nbsp;&nbsp;<a href="javascript:;">Edit</a>'
+            }
+
+            html_str = html_str + '</td></tr>';
+        }
+        html_str = html_str + '</tbody>';
+        table.innerHTML = html_str;
+    }
+}
+
+async function applyCourse(classid, userid, curr,max,vouchers){
+    if( curr >= max){
+        alert("This class is full !")
+    }else if( vouchers < 1){
+        alert("You don't have enough vouchers !")
+    }else{
+        await update_vouchers(userid,vouchers);
+        await update_vacancies(classid, curr);
+        alert("Booking success");
+        window.location = '/';
+    }
+}
+
+function retriev_class () { // Run this function when onload
+    return new Promise((resolve) => {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.open("GET", "retrieve_class", true);
+            xmlhttp.send();
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    //Retrieve data from database
+                    //xmlhttp.response = data
+                    //document.getElementById("class_tabl
+                    console.log("1, ", xmlhttp.responseText)
+                    resolve(xmlhttp.responseText);
+                }
+                else if (xmlhttp.readyState == 4 && xmlhttp.status == 400) {
+                    resolve("Error");
+                }
+            }
+      });
+};
+
+
+
+function update_vouchers (userid,vouchers) { // Run this function when onload
+    return new Promise((resolve) => {
+        var new_vouchers = vouchers - 1;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", `update_vouchers?userid=${userid}&vouchers=${new_vouchers}`, true);
+        xmlhttp.setRequestHeader("Content-type", "application/json");
+        xmlhttp.send();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                console.log("Update vouchers Success");
+                resolve();
+            }
+            else if (xmlhttp.readyState == 4 && xmlhttp.status == 500) {
+                console.log("Update vouchers Fail");
+                resolve();
+            }
+        }
+      });
+};
+
+function reset_cookies(userid) {
+    return new Promise((resolve) => {
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", `reset_cookies?userid=${userid}`, true);
+        xmlhttp.setRequestHeader("Content-type", "application/json");
+        xmlhttp.send();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                console.log("Reset cookies Success");
+                const cookie_list = getCookieList();
+                var vouchers = document.getElementById('vouchers');
+                vouchers.innerHTML = "";
+                vouchers.innerHTML = '<h2 style="text-align: center;"> Voucher(s): ' + cookie_list.vouchers +  '</h2>';
+                resolve();
+            }
+            else if (xmlhttp.readyState == 4 && xmlhttp.status == 500) {
+                console.log("Reset cookies fail")
+                resolve();
+            }
+        }
+    });
+}
+
+function update_vacancies (classid,curr) { // Run this function when onload
+    return new Promise((resolve) => {
+        var new_curr = curr + 1;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("GET", `update_vacancies?classid=${classid}&vacancies=${new_curr}`, true);
+        xmlhttp.setRequestHeader("Content-type", "application/json");
+        xmlhttp.send();
+        xmlhttp.onreadystatechange = function () {
+            if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                console.log("Update vacancies Success");
+                resolve();
+            }
+            else if (xmlhttp.readyState == 4 && xmlhttp.status == 500) {
+                console.log("Update vacancies Fail");
+                resolve();
+            }
+        }
+      });
+};
